a7b77e9db6035e81164e4bff11720aab6957d802
franzqat <franz.f1032@gmail.com>
2019-09-16 15:30:18 +0200

    aggiunto modulo initializer

--- src/node/TripRequestSubmitter.cc
+++ src/node/TripRequestSubmitter.cc
@@ -69,9 +69,6 @@ protected:
 	virtual TripRequest* buildRedCodeRequest(); 	// Request builder
 
 	void buildEmergencySchedule(int totalEmergencies); // Build the emergency schedule vector
-	bool disconnectChannelsAndCheckRedzone();
-
-	bool propagateDistance(int distance);	//Destroy link propagation function
 	void scheduleEmergencyOrRedCode();//Schedule the next emergency as normal or red code
 };
 
@@ -118,159 +115,8 @@ void TripRequestSubmitter::buildEmergencySchedule(int totalEmergencies) {
 	}
 
 	sort(scheduledEmergencies.begin(), scheduledEmergencies.end());
-
-	// print
-//	for (auto elem : v)
-//		ev << elem << "   ";
-//	ev << endl;
 }
 
-/**
- * Return true if the value between 0 and (4 * distance) - 4 is == 0
- * -4 because we always want that an epicenter destroys every links
- *
- */
-bool TripRequestSubmitter::propagateDistance(int distance) {
-	return intuniform(0, 4 * (distance) - 4) == 0;
-}
-
-/*
- * Disconnect channels
- * Check if the node is in redzone and return the bool
- *
- */
-bool TripRequestSubmitter::disconnectChannelsAndCheckRedzone() {
-
-	bool hospital = netmanager->checkHospitalNode(myAddress);
-	bool storagePoint = netmanager->checkStoragePointNode(myAddress);
-	bool collectionPoint = netmanager->checkCollectionPointNode(myAddress);
-
-	bool redZoneNode = false;
-
-	int rows; //of the grid
-	rows = getParentModule()->getParentModule()->par("width");
-	int myX = myAddress % rows;
-	int myY = myAddress / rows;
-
-	// Destroying nodes part
-	cTopology *topo = netmanager->getTopo();
-
-	cTopology::Node* node = topo->getNode(myAddress);
-
-	std::set<int> setOfEpicenters = netmanager->getSetOfEpicenters();
-
-	// Each nodes has a chance of destroying the links, for each epicenter, based on the distance from him and the epicenter.
-	for (auto elem : setOfEpicenters) {
-		int epicX = elem % rows;
-		int epicY = elem / rows;
-
-		if (!hospital && !collectionPoint && !storagePoint) {
-
-			int distance;
-
-			for (int j = 0; j < node->getNumOutLinks(); j++) { //Looking at out connections
-
-				switch (node->getLinkOut(j)->getLocalGate()->getIndex()) {
-
-				case 1:  	// 1 is EAST
-					distance = netmanager->getManhattanDistance(myAddress, elem);
-
-					if (myX >= epicX) // if the node is >= the epicenter, since we're breaking right, it's like we do an hop more
-						distance++;
-
-					if (propagateDistance(distance)) { //if the propagation function returns true, it disconnects the node
-
-						cGate *gate = node->getLinkOut(j)->getLocalGate();
-						gate->disconnect();
-						netmanager->insertRedZoneNode(myAddress);
-						redZoneNode = true;
-
-					}
-					break;
-				case 2: 	// SOUTH
-					distance = netmanager->getManhattanDistance(myAddress, elem);
-
-					if (myY >= epicY) // if the node is >= the epicenter, since we're breaking south, it's like we do an hop more
-						distance++;
-
-					if (propagateDistance(distance)) {
-
-						cGate *gate = node->getLinkOut(j)->getLocalGate();
-						gate->disconnect();
-						netmanager->insertRedZoneNode(myAddress);
-						redZoneNode = true;
-					}
-					break;
-
-				default:
-					break;
-				}
-			}
-
-		}
-
-		// Simmetric check to link in
-		int guardiaW = -1;
-		int guardiaN = -1;
-
-		for (int j = 0; j < node->getNumInLinks(); j++) {
-//			ev << "index del nodo" << node->getModule()->getIndex() << " : in: " << node->getLinkIn(j)->getLocalGate()->getIndex() << endl;
-
-			if (node->getLinkIn(j)->getLocalGate()->getIndex() == 3 && node->getLinkIn(j)->getLocalGate()->isConnected()) {
-				guardiaW = 1; //channel exists, nothing happens
-
-			}
-			if (node->getLinkIn(j)->getLocalGate()->getIndex() == 0 && node->getLinkIn(j)->getLocalGate()->isConnected()) {
-				guardiaN = 1; //channel exists, nothing happens
-
-			}
-
-		}
-		if (guardiaW == -1) {
-
-			//if there isn't a link in, destroy the link out on West
-			for (int k = 0; k < node->getNumOutLinks(); k++) {
-				if (node->getLinkOut(k)->getLocalGate()->getIndex() == 3) {	// check if there's a linkout to a node
-					cGate *gate = node->getLinkOut(k)->getLocalGate();
-					gate->disconnect();
-					netmanager->insertRedZoneNode(myAddress);
-					redZoneNode = true;
-
-				}
-			}
-		}
-		if (guardiaN == -1) {
-
-			//if there isn't a link in, destroy the link out on North
-			for (int k = 0; k < node->getNumOutLinks(); k++) {
-				if (node->getLinkOut(k)->getLocalGate()->getIndex() == 0) {
-					cGate *gate = node->getLinkOut(k)->getLocalGate();
-					gate->disconnect();
-					netmanager->insertRedZoneNode(myAddress);
-					redZoneNode = true;
-				}
-			}
-		}
-	}
-
-	int disconnected = 0; //the node starts connected
-	for (int k = 0; k < node->getNumOutLinks(); k++) { //check out every link out
-
-		cGate *gate = node->getLinkOut(k)->getLocalGate();
-
-		if (!gate->isConnected()) { //count the number of disconnected gates
-			disconnected++;
-		}
-	}
-
-	if (disconnected == node->getNumOutLinks()) { //if the number of disconnected gates is equal to the number of the out links
-		netmanager->insertDestroyedNode(myAddress);	// put the node in the destroyed set
-		netmanager->removeRedZoneNode(myAddress);	// remove it from red zone
-		redZoneNode = false;
-	}
-
-	return redZoneNode;
-}
 
 /**
  * Schedule the next emergency as:
@@ -315,30 +161,14 @@ void TripRequestSubmitter::initialize() {
 		scheduleAt(sendIATime->doubleValue(), truckPacket);
 	}
 
-	bool redZoneNode = disconnectChannelsAndCheckRedzone();
-//	for (auto n : v)
-//		ev << n << endl;
-
-	if (redZoneNode && !netmanager->checkHospitalNode(myAddress)) {
-
+	if (netmanager->checkRedZoneNode(myAddress) && !netmanager->checkHospitalNode(myAddress)) {
 		totalEmergenciesPerNode = par("numberOfEmergencies");
 		emergencyIndex = 0;
-
 		buildEmergencySchedule(totalEmergenciesPerNode);
 
 		scheduleEmergencyOrRedCode();
-
-		if (ev.isGUI())
-			getParentModule()->getDisplayString().setTagArg("b", 3, "red");
-	}
-	if (netmanager->checkDestroyedNode(myAddress)) { // Look and feel
-		if (ev.isGUI()) {
-			getParentModule()->getDisplayString().setTagArg("b", 0, "0");
-			getParentModule()->getDisplayString().setTagArg("b", 1, "0");
-		}
 	}
 
-	netmanager->updateTopology(); //updates the topology in network manager
 }
 
 void TripRequestSubmitter::handleMessage(cMessage *msg) {
